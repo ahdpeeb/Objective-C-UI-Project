dcf9102942f6d0da9300722c186350793546c7e3
ahdpeeb <ahdpeeb.h@gmail.com>
2016-07-19 14:41:18 +0300

    Project initialized

---
+++ Objective-C UI Project/Source/FirstTask/ANSLableView.h
@@ -0,0 +1,13 @@
+//
+//  ANSLableView.h
+//  Objective-C UI Project
+//
+//  Created by Nikola Andriiev on 19.07.16.
+//  Copyright © 2016 Andriiev.Mykola. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ANSLableView : UIView
+
+@end
---
+++ Objective-C UI Project/Source/FirstTask/ViewController/ANSViewControllerFirstTask.m
@@ -0,0 +1,37 @@
+//
+//  ANSViewControllerFirstTask.m
+//  Objective-C UI Project
+//
+//  Created by Nikola Andriiev on 19.07.16.
+//  Copyright © 2016 Andriiev.Mykola. All rights reserved.
+//
+
+#import "ANSViewControllerFirstTask.h"
+
+@interface ANSViewControllerFirstTask ()
+
+@end
+
+@implementation ANSViewControllerFirstTask
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
