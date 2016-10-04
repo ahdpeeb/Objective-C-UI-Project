9f9c367219acad2560cf1d2fb5617f924313b7d0
ahdpeeb <ahdpeeb.h@gmail.com>
2016-10-04 12:38:17 +0300

    file system correction

---
+++ Objective-C UI Project/Library/Models/ANSNameFilterModel/ANSNameFilterModel.h
@@ -0,0 +1,35 @@
+//
+//  ANSNameFilterModel.h
+//  Objective-C UI Project
+//
+//  Created by Nikola Andriiev on 30.08.16.
+//  Copyright © 2016 Andriiev.Mykola. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "ANSArrayModel.h"
+#import "ANSFBFriends.h"
+
+@class ANSNameFilterModel;
+
+@protocol ANSNameFilterModelProtocol <ANSArrayModelObserver>
+
+- (void)nameFilterModelDidFilter:(ANSNameFilterModel *)model;
+
+@end
+
+typedef NS_ENUM(NSUInteger, ANSNameFilterModelState) {
+    ANSNameFilterModelDidFilter = ANSArrayModelStatesCount,
+    
+    ANSNameFilterModelCount
+};
+
+@interface ANSNameFilterModel : ANSArrayModel <ANSArrayModelObserver>
+@property (nonatomic, readonly) id observableObject;
+
+- (instancetype)initWithObservableModel:(id)model;
+
+- (void)filterByfilterString:(NSString *)filterString;
+
+@end
